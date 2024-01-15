f28164550e0ab5a3be1f56def93f16ec7c77f5dc
Shawon <chiefmahedi@gmail.com>
2024-01-15 23:06:17 +0600

    Add string related code

---
+++ Intermediate/Strings/strings.mjs
@@ -0,0 +1,11 @@
+const s1 = 'Chief';
+console.log(s1);
+const s2 = "Mahedi";
+console.log(s2);
+const s3 = "\xA9";
+console.log(s3);
+const s4 = "\u00A9";
+console.log(s4);
+const stringObj = new String("This is a string object"); 
+console.log(stringObj);
+console.log(typeof stringObj);
